adef96882f9c74a7f5b5fbcbae70376d33ee660a
Tom Close <tom.g.close@gmail.com>
2025-04-16 22:40:00 +1000

    added generators for all siemens raw data types

--- extras/fileformats/extras/medimage/raw.py
+++ extras/fileformats/extras/medimage/raw.py
@@ -2,6 +2,7 @@ import typing as ty
 from pathlib import Path
 import pydicom
 import sys
+from collections import Counter
 from fileformats.core import SampleFileGenerator
 from medimages4tests.dummy.raw.pet.siemens.biograph_vision.vr20b.pet_listmode import (
     get_data as get_pet_listmode_data,
@@ -9,13 +10,32 @@ from medimages4tests.dummy.raw.pet.siemens.biograph_vision.vr20b.pet_listmode im
 from medimages4tests.dummy.raw.pet.siemens.biograph_vision.vr20b.pet_countrate import (
     get_data as get_pet_countrate_data,
 )
+from medimages4tests.dummy.raw.pet.siemens.biograph_vision.vr20b.pet_em_sino import (
+    get_data as get_pet_sinogram_data,
+)
+from medimages4tests.dummy.raw.pet.siemens.biograph_vision.vr20b.pet_calibration import (
+    get_data as get_pet_calibration_data,
+)
+from medimages4tests.dummy.raw.pet.siemens.biograph_vision.vr20b.pet_dynamics_sino import (
+    get_data as get_pet_dynamics_sino_data,
+)
+from medimages4tests.dummy.raw.pet.siemens.biograph_vision.vr20b.pet_replay_param import (
+    get_data as get_pet_replay_param_data,
+)
+from medimages4tests.dummy.raw.pet.siemens.biograph_vision.vr20b.petct_spl import (
+    get_data as get_petct_spl_data,
+)
 from fileformats.core import extra_implementation, FileSet
 from fileformats.medimage.dicom import DicomImage
 from fileformats.medimage.raw import (
     Vnd_Siemens_Biograph128Vision_Vr20b_PetRawData,
     Vnd_Siemens_Biograph128Vision_Vr20b_LargePetRawData,
-    Vnd_Siemens_Biograph128Vision_Vr20b_PetCountRate,
     Vnd_Siemens_Biograph128Vision_Vr20b_PetListMode,
+    Vnd_Siemens_Biograph128Vision_Vr20b_PetSinogram,
+    Vnd_Siemens_Biograph128Vision_Vr20b_PetDynamicSinogramSeries,
+    Vnd_Siemens_Biograph128Vision_Vr20b_PetCountRate,
+    Vnd_Siemens_Biograph128Vision_Vr20b_PetNormalisation,
+    Vnd_Siemens_Biograph128Vision_Vr20b_PetParameterisation,
     Vnd_Siemens_Biograph128Vision_Vr20b_PetCtSplRawData,
 )
 from fileformats.core.io import BinaryIOWindow
@@ -99,6 +119,43 @@ def siemens_petct_raw_data_load_pydicom(
     return dcm
 
 
+@extra_implementation(FileSet.read_metadata)
+def siemens_pet_dynamic_sinogram_series_read_metadata(
+    pet_raw_data: Vnd_Siemens_Biograph128Vision_Vr20b_PetDynamicSinogramSeries,
+    specific_tags: ty.Optional[TagListType] = None,
+    **kwargs: ty.Any,
+) -> ty.Mapping[str, ty.Any]:
+
+    # Collated DICOM headers across series
+    collated: ty.Dict[str, ty.Any] = {}
+    key_repeats: ty.Counter[str] = Counter()
+    varying_keys = set()
+    # We use the "contents" property implementation in TypeSet instead of the overload
+    # in DicomCollection because we don't want the metadata to be read ahead of the
+    # the `select_metadata` call below
+
+    for ptd in pet_raw_data.contents:
+        for key, val in ptd.metadata.items():
+            try:
+                prev_val = collated[key]
+            except KeyError:
+                collated[
+                    key
+                ] = val  # Insert initial value (should only happen on first iter)
+                key_repeats.update([key])
+            else:
+                if key in varying_keys:
+                    collated[key].append(val)
+                # Check whether the value is the same as the values in the previous
+                # images in the series
+                elif val != prev_val:
+                    collated[key] = [prev_val] * key_repeats[key] + [val]
+                    varying_keys.add(key)
+                else:
+                    key_repeats.update([key])
+    return collated
+
+
 @extra_implementation(FileSet.generate_sample_data)
 def siemens_pet_listmode_generate_sample_data(
     pet_raw_data: Vnd_Siemens_Biograph128Vision_Vr20b_PetListMode,
@@ -113,3 +170,43 @@ def siemens_pet_countrate_generate_sample_data(
     generator: SampleFileGenerator,
 ) -> ty.List[Path]:
     return get_pet_countrate_data(out_dir=generator.dest_dir)  # type: ignore[no-any-return]
+
+
+@extra_implementation(FileSet.generate_sample_data)
+def siemens_pet_sinogram_generate_sample_data(
+    pet_raw_data: Vnd_Siemens_Biograph128Vision_Vr20b_PetSinogram,
+    generator: SampleFileGenerator,
+) -> ty.List[Path]:
+    return get_pet_sinogram_data(out_dir=generator.dest_dir)  # type: ignore[no-any-return]
+
+
+@extra_implementation(FileSet.generate_sample_data)
+def siemens_pet_dynamics_sino_generate_sample_data(
+    pet_raw_data: Vnd_Siemens_Biograph128Vision_Vr20b_PetDynamicSinogramSeries,
+    generator: SampleFileGenerator,
+) -> ty.List[Path]:
+    return get_pet_dynamics_sino_data(out_dir=generator.dest_dir)  # type: ignore[no-any-return]
+
+
+@extra_implementation(FileSet.generate_sample_data)
+def siemens_pet_normalisation_generate_sample_data(
+    pet_raw_data: Vnd_Siemens_Biograph128Vision_Vr20b_PetNormalisation,
+    generator: SampleFileGenerator,
+) -> ty.List[Path]:
+    return get_pet_calibration_data(out_dir=generator.dest_dir)  # type: ignore[no-any-return]
+
+
+@extra_implementation(FileSet.generate_sample_data)
+def siemens_petct_spl_generate_sample_data(
+    pet_raw_data: Vnd_Siemens_Biograph128Vision_Vr20b_PetCtSplRawData,
+    generator: SampleFileGenerator,
+) -> ty.List[Path]:
+    return get_petct_spl_data(out_dir=generator.dest_dir)  # type: ignore[no-any-return]
+
+
+@extra_implementation(FileSet.generate_sample_data)
+def siemens_pet_parameterisation_generate_sample_data(
+    pet_raw_data: Vnd_Siemens_Biograph128Vision_Vr20b_PetParameterisation,
+    generator: SampleFileGenerator,
+) -> ty.List[Path]:
+    return get_pet_replay_param_data(out_dir=generator.dest_dir)  # type: ignore[no-any-return]
--- extras/fileformats/extras/medimage/tests/test_generators.py
+++ extras/fileformats/extras/medimage/tests/test_generators.py
@@ -6,6 +6,11 @@ from fileformats.medimage import (
     Fmri,
     Vnd_Siemens_Biograph128Vision_Vr20b_PetListMode,
     Vnd_Siemens_Biograph128Vision_Vr20b_PetCountRate,
+    Vnd_Siemens_Biograph128Vision_Vr20b_PetSinogram,
+    Vnd_Siemens_Biograph128Vision_Vr20b_PetDynamicSinogramSeries,
+    Vnd_Siemens_Biograph128Vision_Vr20b_PetNormalisation,
+    Vnd_Siemens_Biograph128Vision_Vr20b_PetParameterisation,
+    Vnd_Siemens_Biograph128Vision_Vr20b_PetCtSplRawData,
 )
 
 
@@ -31,4 +36,29 @@ def test_siemens_pet_listmode_generator():
 
 def test_siemens_pet_countrate_generator():
     img = Vnd_Siemens_Biograph128Vision_Vr20b_PetCountRate.sample()
+    assert img.metadata["PatientName"] == "FirstName^LastName"
+
+
+def test_siemens_pet_sinogram_generator():
+    img = Vnd_Siemens_Biograph128Vision_Vr20b_PetSinogram.sample()
+    assert img.metadata["PatientName"] == "FirstName^LastName"
+
+
+def test_siemens_pet_dynamics_sino_generator():
+    img = Vnd_Siemens_Biograph128Vision_Vr20b_PetDynamicSinogramSeries.sample()
+    assert img.metadata["PatientName"] == "FirstName^LastName"
+
+
+def test_siemens_pet_normalisation_generator():
+    img = Vnd_Siemens_Biograph128Vision_Vr20b_PetNormalisation.sample()
+    assert img.metadata["PatientName"] == "FirstName^LastName"
+
+
+def test_siemens_pet_petct_spl_generator():
+    img = Vnd_Siemens_Biograph128Vision_Vr20b_PetCtSplRawData.sample()
+    assert img.metadata["PatientName"] == "FirstName^LastName"
+
+
+def test_siemens_pet_parameterisation_generator():
+    img = Vnd_Siemens_Biograph128Vision_Vr20b_PetParameterisation.sample()
     assert img.metadata["PatientName"] == "FirstName^LastName"
