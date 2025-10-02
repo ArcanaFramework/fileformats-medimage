48dbaae47263db40f21655747c8bd8b476272b4a
Thomas G. Close <tom.g.close@gmail.com>
2025-10-02 16:16:42 +1000

    split out Siemens formats into separate package

--- fileformats/medimage/raw/__init__.py
+++ fileformats/medimage/raw/__init__.py
@@ -8,16 +8,6 @@ from .pet import (
     PetSinogram,
     PetCountRate,
     PetNormalisation,
-    Vnd_Siemens_Biograph128Vision_Vr20b_PetRawData,
-    Vnd_Siemens_Biograph128Vision_Vr20b_LargePetRawData,
-    Vnd_Siemens_Biograph128Vision_Vr20b_PetListMode,
-    Vnd_Siemens_Biograph128Vision_Vr20b_PetSinogram,
-    Vnd_Siemens_Biograph128Vision_Vr20b_PetDynamicSinogram,
-    Vnd_Siemens_Biograph128Vision_Vr20b_PetDynamicSinogramSeries,
-    Vnd_Siemens_Biograph128Vision_Vr20b_PetCountRate,
-    Vnd_Siemens_Biograph128Vision_Vr20b_PetNormalisation,
-    Vnd_Siemens_Biograph128Vision_Vr20b_PetParameterisation,
-    Vnd_Siemens_Biograph128Vision_Vr20b_PetCtSpl,
 )
 
 __all__ = [
@@ -28,14 +18,4 @@ __all__ = [
     "PetSinogram",
     "PetCountRate",
     "PetNormalisation",
-    "Vnd_Siemens_Biograph128Vision_Vr20b_PetRawData",
-    "Vnd_Siemens_Biograph128Vision_Vr20b_LargePetRawData",
-    "Vnd_Siemens_Biograph128Vision_Vr20b_PetListMode",
-    "Vnd_Siemens_Biograph128Vision_Vr20b_PetSinogram",
-    "Vnd_Siemens_Biograph128Vision_Vr20b_PetDynamicSinogram",
-    "Vnd_Siemens_Biograph128Vision_Vr20b_PetDynamicSinogramSeries",
-    "Vnd_Siemens_Biograph128Vision_Vr20b_PetCountRate",
-    "Vnd_Siemens_Biograph128Vision_Vr20b_PetNormalisation",
-    "Vnd_Siemens_Biograph128Vision_Vr20b_PetParameterisation",
-    "Vnd_Siemens_Biograph128Vision_Vr20b_PetCtSpl",
 ]
