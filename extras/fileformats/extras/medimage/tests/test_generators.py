dfade83e82f2a3f394acebe307882a66ba4df947
Thomas G. Close <tom.g.close@gmail.com>
2025-10-08 09:54:48 +1100

    stripped out Siemens raw data formats

--- extras/fileformats/extras/medimage/tests/test_generators.py
+++ extras/fileformats/extras/medimage/tests/test_generators.py
@@ -4,13 +4,13 @@ from fileformats.medimage import (
     T1w,
     Dmri,
     Fmri,
-    Vnd_Siemens_Biograph128Vision_Vr20b_PetListMode,
-    Vnd_Siemens_Biograph128Vision_Vr20b_PetCountRate,
-    Vnd_Siemens_Biograph128Vision_Vr20b_PetSinogram,
-    Vnd_Siemens_Biograph128Vision_Vr20b_PetDynamicSinogramSeries,
-    Vnd_Siemens_Biograph128Vision_Vr20b_PetNormalisation,
-    Vnd_Siemens_Biograph128Vision_Vr20b_PetParameterisation,
-    Vnd_Siemens_Biograph128Vision_Vr20b_PetCtSpl,
+    # Vnd_Siemens_Biograph128Vision_Vr20b_PetListMode,
+    # Vnd_Siemens_Biograph128Vision_Vr20b_PetCountRate,
+    # Vnd_Siemens_Biograph128Vision_Vr20b_PetSinogram,
+    # Vnd_Siemens_Biograph128Vision_Vr20b_PetDynamicSinogramSeries,
+    # Vnd_Siemens_Biograph128Vision_Vr20b_PetNormalisation,
+    # Vnd_Siemens_Biograph128Vision_Vr20b_PetParameterisation,
+    # Vnd_Siemens_Biograph128Vision_Vr20b_PetCtSpl,
 )
 
 
@@ -29,36 +29,36 @@ def test_dmri_generator():
     assert len(img.dims()) == 4
 
 
-def test_siemens_pet_listmode_generator():
-    img = Vnd_Siemens_Biograph128Vision_Vr20b_PetListMode.sample()
-    assert img.metadata["PatientName"] == "FirstName^LastName"
+# def test_siemens_pet_listmode_generator():
+#     img = Vnd_Siemens_Biograph128Vision_Vr20b_PetListMode.sample()
+#     assert img.metadata["PatientName"] == "FirstName^LastName"
 
 
-def test_siemens_pet_countrate_generator():
-    img = Vnd_Siemens_Biograph128Vision_Vr20b_PetCountRate.sample()
-    assert img.metadata["PatientName"] == "FirstName^LastName"
+# def test_siemens_pet_countrate_generator():
+#     img = Vnd_Siemens_Biograph128Vision_Vr20b_PetCountRate.sample()
+#     assert img.metadata["PatientName"] == "FirstName^LastName"
 
 
-def test_siemens_pet_sinogram_generator():
-    img = Vnd_Siemens_Biograph128Vision_Vr20b_PetSinogram.sample()
-    assert img.metadata["PatientName"] == "FirstName^LastName"
+# def test_siemens_pet_sinogram_generator():
+#     img = Vnd_Siemens_Biograph128Vision_Vr20b_PetSinogram.sample()
+#     assert img.metadata["PatientName"] == "FirstName^LastName"
 
 
-def test_siemens_pet_dynamics_sino_generator():
-    img = Vnd_Siemens_Biograph128Vision_Vr20b_PetDynamicSinogramSeries.sample()
-    assert img.metadata["PatientName"] == "FirstName^LastName"
+# def test_siemens_pet_dynamics_sino_generator():
+#     img = Vnd_Siemens_Biograph128Vision_Vr20b_PetDynamicSinogramSeries.sample()
+#     assert img.metadata["PatientName"] == "FirstName^LastName"
 
 
-def test_siemens_pet_normalisation_generator():
-    img = Vnd_Siemens_Biograph128Vision_Vr20b_PetNormalisation.sample()
-    assert img.metadata["PatientName"] == "FirstName^LastName"
+# def test_siemens_pet_normalisation_generator():
+#     img = Vnd_Siemens_Biograph128Vision_Vr20b_PetNormalisation.sample()
+#     assert img.metadata["PatientName"] == "FirstName^LastName"
 
 
-def test_siemens_pet_petct_spl_generator():
-    img = Vnd_Siemens_Biograph128Vision_Vr20b_PetCtSpl.sample()
-    assert img.metadata["PatientName"] == "FirstName^LastName"
+# def test_siemens_pet_petct_spl_generator():
+#     img = Vnd_Siemens_Biograph128Vision_Vr20b_PetCtSpl.sample()
+#     assert img.metadata["PatientName"] == "FirstName^LastName"
 
 
-def test_siemens_pet_parameterisation_generator():
-    img = Vnd_Siemens_Biograph128Vision_Vr20b_PetParameterisation.sample()
-    assert img.metadata["PatientName"] == "FirstName^LastName"
+# def test_siemens_pet_parameterisation_generator():
+#     img = Vnd_Siemens_Biograph128Vision_Vr20b_PetParameterisation.sample()
+#     assert img.metadata["PatientName"] == "FirstName^LastName"
