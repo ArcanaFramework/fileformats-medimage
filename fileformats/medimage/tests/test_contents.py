14f163d58bef75e295a1386497d4303aac96f0d1
Thomas G. Close <tom.g.close@gmail.com>
2026-05-29 10:16:55 +1000

    mark xfail

--- fileformats/medimage/tests/test_contents.py
+++ fileformats/medimage/tests/test_contents.py
@@ -13,6 +13,6 @@ def test_image_contents1():
 
 
 @pytest.mark.xfail(reason="There is a bug in medimages4tests")
-def test_image_contents_generation():
+def test_image_contents_generation() -> None:
     img = NiftiGzX[T1w].sample()
     assert len(img.dims()) == 3
